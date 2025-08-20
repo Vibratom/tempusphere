f06d8315d64aea1a0b7a883d5fa53455c13b64d2
Vibratom Studios <vibratomofficial@gmail.com>
2025-08-20 12:09:24 +0000

    for some reason the favison is not changing and its showing firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/logo.webp" type="image/webp" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
