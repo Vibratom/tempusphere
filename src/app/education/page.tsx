b4a9648c2ba2f4f200b03fc1b1d8a1921140ed51
Vibratom Studios <vibratomofficial@gmail.com>
2025-09-18 08:16:36 +0000

    remove the embedded code thing we will create our own here. we will crea

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -6,9 +6,7 @@ import { Header } from '@/components/tempusphere/Header';
 import { Footer } from '@/components/tempusphere/Footer';
 import { SettingsProvider } from '@/contexts/SettingsContext';
 import { WebApisView } from '@/components/education/WebApisView';
-import { EmbedView } from '@/components/education/EmbedView';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { BookOpen, BookCopy } from 'lucide-react';
+import { BookOpen } from 'lucide-react';
 
 function EducationContent() {
   return (
@@ -17,18 +15,7 @@ function EducationContent() {
         <Header />
         <main className="flex-1 flex flex-col items-center p-4 md:p-8">
             <div className="w-full max-w-5xl">
-                 <Tabs defaultValue="web-apis" className="w-full">
-                    <TabsList className="grid w-full grid-cols-2">
-                        <TabsTrigger value="web-apis"><BookOpen className="mr-2"/>Reference</TabsTrigger>
-                        <TabsTrigger value="interactive"><BookCopy className="mr-2"/>Interactive</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="web-apis" className="mt-6">
-                        <WebApisView />
-                    </TabsContent>
-                    <TabsContent value="interactive" className="mt-6">
-                        <EmbedView />
-                    </TabsContent>
-                </Tabs>
+                <WebApisView />
             </div>
         </main>
         <Footer />
