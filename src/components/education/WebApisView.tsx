86644b463cef7f500e3722bbef29a8f8deb4b147
Vibratom Studios <vibratomofficial@gmail.com>
2025-09-18 08:18:13 +0000

    in education merge dictionary and translator

--- src/components/education/WebApisView.tsx
+++ src/components/education/WebApisView.tsx
@@ -2,22 +2,16 @@
 'use client';
 
 import { Card } from '../ui/card';
-import { DictionaryApp } from './DictionaryApp';
 import { CountryExplorer } from './CountryExplorer';
 import { HistoricalEvents } from './HistoricalEvents';
-import { TranslatorApp } from './TranslatorApp';
+import { LanguageTools } from './LanguageTools';
 
 export function WebApisView() {
     return (
         <div className="space-y-8">
-            <div className="grid grid-cols-1 xl:grid-cols-2 gap-8">
-                <Card>
-                    <DictionaryApp />
-                </Card>
-                <Card>
-                    <TranslatorApp />
-                </Card>
-            </div>
+            <Card>
+                <LanguageTools />
+            </Card>
             <Card>
                 <CountryExplorer />
             </Card>
