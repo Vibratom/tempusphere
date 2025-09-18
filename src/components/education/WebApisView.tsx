f0986ead4c70759859f3baf1bff3578eea99cd0c
Vibratom Studios <vibratomofficial@gmail.com>
2025-09-18 07:53:56 +0000

    want it without AI powered. which will tranlate only words

--- src/components/education/WebApisView.tsx
+++ src/components/education/WebApisView.tsx
@@ -1,16 +1,23 @@
+
 'use client';
 
 import { Card } from '../ui/card';
 import { DictionaryApp } from './DictionaryApp';
 import { CountryExplorer } from './CountryExplorer';
 import { HistoricalEvents } from './HistoricalEvents';
+import { TranslatorApp } from './TranslatorApp';
 
 export function WebApisView() {
     return (
         <div className="space-y-8">
-            <Card>
-                <DictionaryApp />
-            </Card>
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-8">
+                <Card>
+                    <DictionaryApp />
+                </Card>
+                <Card>
+                    <TranslatorApp />
+                </Card>
+            </div>
             <Card>
                 <CountryExplorer />
             </Card>
