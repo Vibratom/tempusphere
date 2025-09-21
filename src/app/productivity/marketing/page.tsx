b634c2f1c3d4ca3dbb8853faef6ae6bec3af110c
Vibratom Studios <vibratomofficial@gmail.com>
2025-09-21 13:50:17 +0000

    Try fixing this error: `Runtime Error: Error: The default export is not

--- src/app/productivity/marketing/page.tsx
+++ src/app/productivity/marketing/page.tsx
@@ -1,5 +1,12 @@
-
 'use client';
+
+import { useEffect } from 'react';
 import { redirect } from 'next/navigation';
 
-export default function MarketingRedirectPage()
+export default function MarketingRedirectPage() {
+  useEffect(() => {
+    redirect('/productivity/marketing/channels');
+  }, []);
+
+  return null;
+}
